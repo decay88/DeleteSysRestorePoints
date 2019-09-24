6e277c2bd4161576515f41d0a141ada25727d826
r3xq1 <40786755+r3xq1@users.noreply.github.com>
2019-09-24 12:37:22 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace RestorePoints
+{
+    using System;
+
+    /* Автор r3xq1 */
+
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            SysStore.Inizialize();
+            Console.ReadKey(true);
+        }
+    }
+}
